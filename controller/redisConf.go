7c09e91bd393bd5359d50b63cad6d1e94b7d09b0
周zhou <1947634249@qq.com>
2021-04-24 21:13:44 +0800

    Default Changelist

---
+++ controller/redisConf.go
@@ -0,0 +1,66 @@
+package controller
+
+import (
+	"getaway/dao/redis"
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func UpdatesubScribeMsg(c *gin.Context) {
+	data := c.Param("data")
+	log.Println(data)
+	result, err := redis.RedisClient.Set("subScribeMsg", data, -1).Result()
+	if err != nil {
+		log.Printf("subScribeMsg set error, err(%v)", err)
+		ResponseErrorWithMsg(c, CodeServerBusy, "服务器错误")
+
+	}
+	ResponseSuccess(c, result)
+}
+
+//r.PUT("api/v1/UpdateGroupQrCode/:data", controller.UpdatesubScribeMsg)
+//r.PUT("api/v1/UpdateCustomerMsg/:data", controller.UpdatesubScribeMsg)
+//r.PUT("api/v1/UpdateCustomerImgMsg/:data", controller.UpdatesubScribeMsg)
+func UpdateGroupQrCode(c *gin.Context) {
+	data := c.Param("data")
+	log.Println(data)
+	result, err := redis.RedisClient.Set("groupQrCode", data, -1).Result()
+	if err != nil {
+		log.Printf("groupQrCode set error, err(%v)", err)
+		ResponseErrorWithMsg(c, CodeServerBusy, "服务器错误")
+	}
+	ResponseSuccess(c, result)
+}
+
+func UpdateCustomerMsg(c *gin.Context) {
+	data := c.Param("data")
+	log.Println(data)
+	result, err := redis.RedisClient.Set("customerMsg", data, -1).Result()
+	if err != nil {
+		log.Printf("customerMsg set error, err(%v)", err)
+		ResponseErrorWithMsg(c, CodeServerBusy, "服务器错误")
+	}
+	ResponseSuccess(c, result)
+}
+
+func UpdateCustomerImgMsg(c *gin.Context) {
+	data := c.Param("data")
+	log.Println(data)
+	result, err := redis.RedisClient.Set("customerImgMsg", data, -1).Result()
+	if err != nil {
+		log.Printf("customerImgMsg set error, err(%v)", err)
+		ResponseErrorWithMsg(c, CodeServerBusy, "服务器错误")
+	}
+	ResponseSuccess(c, result)
+}
+
+func UpdateSendTimer(c *gin.Context) {
+	data := c.Param("data")
+	log.Println(data)
+	result, err := redis.RedisClient.Set("sendTimer", data, -1).Result()
+	if err != nil {
+		log.Printf("sendTimer set error, err(%v)", err)
+		ResponseErrorWithMsg(c, CodeServerBusy, "服务器错误")
+	}
+	ResponseSuccess(c, result)
+}
---
+++ utils/directory.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"log"
+	"os"
+)
+
+//@description: 文件目录是否存在
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+//@description: 批量创建文件夹
+func CreateDir(dirs ...string) (err error) {
+	for _, v := range dirs {
+		exist, err := PathExists(v)
+		if err != nil {
+			return err
+		}
+		if !exist {
+			err = os.MkdirAll(v, os.ModePerm)
+			if err != nil {
+				log.Printf("create directory error: %v\n", err)
+			}
+		}
+	}
+	return err
+}
